23756b6f3dd5f609e75025647b2d9ae7cf202562
jdu211171 <211171m@jdu.uz>
2025-02-04 02:40:49 +0500

    session refactored

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -32,18 +32,7 @@ return {
     "rmagatti/auto-session",
     lazy = false,
     config = function()
-      require "auto-session".setup({
-        auto_session_suppress_dirs = { "~/", "~/Development", "~/Downloads", "/" },
-        session_lens = {
-          buftypes_to_ignore = {},
-          load_on_setup = true,
-          theme_conf = { border = true },
-          preveiwer = false,
-        },
-        vim.keymap.set("n", "<leader>ls", require("auto-session.session-lens").search_session, {
-        noremap = true
-      }),
-    })
+      require "configs.auto-session"
     end,
   },
 
