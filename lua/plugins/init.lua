ec0c6df4b90192a7edd0bbb4b0bc03020ae11478
jdu211171 <211171m@jdu.uz>
2025-02-04 04:44:06 +0500

    conform fixed

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,8 @@
 return {
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
+    event = 'BufWritePre', -- uncomment for format on save
+    lazy = false,
     opts = require "configs.conform",
   },
 
