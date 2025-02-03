5834e4ed142d10f312e7b875183a6c8712a9216c
jdu211171 <211171m@jdu.uz>
2025-02-04 02:32:30 +0500

    session added

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -27,6 +27,26 @@ return {
     end,
   },
 
+  -- Sessions
+  {
+    "rmagatti/auto-session",
+    lazy = false,
+    config = function()
+      require "auto-session".setup({
+        auto_session_suppress_dirs = { "~/", "~/Development", "~/Downloads", "/" },
+        session_lens = {
+          buftypes_to_ignore = {},
+          load_on_setup = true,
+          theme_conf = { border = true },
+          preveiwer = false,
+        },
+        vim.keymap.set("n", "<leader>ls", require("auto-session.session-lens").search_session, {
+        noremap = true
+      }),
+    })
+    end,
+  },
+
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
