43f45dc455be844006fe484e39a948ad5b9ff6be
jdu211171 <211171m@jdu.uz>
2025-02-14 00:29:46 +0500

    navigate to hunks shortcut

--- lua/configs/gitsigns.lua
+++ lua/configs/gitsigns.lua
@@ -1,6 +1,6 @@
-require('gitsigns').setup{
+require("gitsigns").setup {
   on_attach = function(bufnr)
-    local gitsigns = require('gitsigns')
+    local gitsigns = require "gitsigns"
 
     local function map(mode, l, r, opts)
       opts = opts or {}
@@ -9,58 +9,64 @@ require('gitsigns').setup{
     end
 
     -- Navigation
-    map('n', ']c', function()
+    map("n", "]c", function()
       if vim.wo.diff then
-        vim.cmd.normal({']c', bang = true})
+        vim.cmd.normal { "]c", bang = true }
       else
-        gitsigns.nav_hunk('next')
+        gitsigns.nav_hunk "next"
       end
     end)
 
-    map('n', '[c', function()
+    map("n", "[c", function()
       if vim.wo.diff then
-        vim.cmd.normal({'[c', bang = true})
+        vim.cmd.normal { "[c", bang = true }
       else
-        gitsigns.nav_hunk('prev')
+        gitsigns.nav_hunk "prev"
       end
     end)
 
     -- Actions
-    map('n', '<leader>hs', gitsigns.stage_hunk, { desc = 'Stage hunk' })
-    map('n', '<leader>hr', gitsigns.reset_hunk, { desc = 'Reset hunk' })
+    map("n", "<leader>hs", gitsigns.stage_hunk, { desc = "Stage hunk" })
+    map("n", "<leader>hr", gitsigns.reset_hunk, { desc = "Reset hunk" })
 
-    map('v', '<leader>hs', function()
-      gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+    map("v", "<leader>hs", function()
+      gitsigns.stage_hunk { vim.fn.line ".", vim.fn.line "v" }
     end)
 
-    map('v', '<leader>hr', function()
-      gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+    map("v", "<leader>hr", function()
+      gitsigns.reset_hunk { vim.fn.line ".", vim.fn.line "v" }
     end)
 
-    map('n', '<leader>hS', gitsigns.stage_buffer, { desc = 'Stage buffer' })
-    map('n', '<leader>hR', gitsigns.reset_buffer, { desc = 'Reset buffer' })
-    map('n', '<leader>hp', gitsigns.preview_hunk, { desc = 'Preview hunk' })
-    map('n', '<leader>hi', gitsigns.preview_hunk_inline, { desc = 'Preview hunk inline' })
+    map("n", "<leader>hS", gitsigns.stage_buffer, { desc = "Stage buffer" })
+    map("n", "<leader>hR", gitsigns.reset_buffer, { desc = "Reset buffer" })
+    map("n", "<leader>hp", gitsigns.preview_hunk, { desc = "Preview hunk" })
+    map("n", "<leader>hi", gitsigns.preview_hunk_inline, { desc = "Preview hunk inline" })
 
-    map('n', '<leader>hb', function()
-      gitsigns.blame_line({ full = true })
-    end, { desc = 'Blame line' })
+    map("n", "<leader>hb", function()
+      gitsigns.blame_line { full = true }
+    end, { desc = "Blame line" })
 
-    map('n', '<leader>hd', gitsigns.diffthis, { desc = 'Diffthis' })
+    map("n", "<leader>hd", gitsigns.diffthis, { desc = "Diffthis" })
 
-    map('n', '<leader>hD', function()
-      gitsigns.diffthis('~')
-    end, { desc = 'Diffthis (reverse)' })
+    map("n", "<leader>hD", function()
+      gitsigns.diffthis "~"
+    end, { desc = "Diffthis (reverse)" })
 
-    map('n', '<leader>hQ', function() gitsigns.setqflist('all') end, { desc = 'Set quickfix list' })
-    map('n', '<leader>hq', gitsigns.setqflist, { desc = 'Set quickfix list' })
+    map("n", "<leader>hQ", function()
+      gitsigns.setqflist "all"
+    end, { desc = "Set quickfix list" })
+    map("n", "<leader>hq", gitsigns.setqflist, { desc = "Set quickfix list" })
 
     -- Toggles
-    map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = 'Toggle current line blame' })
-    map('n', '<leader>td', gitsigns.toggle_deleted, { desc = 'Toggle deleted' })
-    map('n', '<leader>tw', gitsigns.toggle_word_diff, { desc = 'Toggle word diff' })
+    map("n", "<leader>tb", gitsigns.toggle_current_line_blame, { desc = "Toggle current line blame" })
+    map("n", "<leader>td", gitsigns.toggle_deleted, { desc = "Toggle deleted" })
+    map("n", "<leader>tw", gitsigns.toggle_word_diff, { desc = "Toggle word diff" })
 
     -- Text object
-    map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
-  end
+    map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+
+    -- Navigate to signs
+    map("n", "]h", gitsigns.next_hunk, { desc = "Next hunk" })
+    map("n", "[h", gitsigns.prev_hunk, { desc = "Previous hunk" })
+  end,
 }
